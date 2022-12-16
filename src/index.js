6a8a2de4d6ad15b7a507f91e52eafb226bb5d925
Justas Lapiene <Justas63@gmail.com>
2022-12-16 15:50:57 +0100

    got the factory function working.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,46 @@
-import './style.scss'
-
+import './style.scss';
+class Project {
+constructor  (title) {
+    this.title = title
+}
+}
 (function() {
+    
     let todoApp = {
-        projectList : [],
+        projectListArr : [],
         toDoList: [],
     init: function() {
         this.cacheDom();
         this.bindEvents();
-        this.shorts();
         this.renderProjectList();
     },
-    shorts: function() {
-        function createDiv() {
-            document.createElement('div')
-        }
-    },
     cacheDom: function() {
-        this.addProject = document.getElementById('add')
+        this.addProjectBtn = document.getElementById('add')
         this.projectList = document.getElementById('projectList')
         this.deleteProject = document.getElementById('delete')
-    },
-    bindEvents: function() {
-        this.addProject.addEventListener('click', (this.addProject))
+        this.div = document.createElement('div')
     },
     addProject: function() {
-        this.createProject();
-        projectList.push(this.project)
+        this.projectListArr.push(this.project)
+    },
+    bindEvents: function() {
+        this.addProjectBtn.addEventListener('click', () => {
+            this.createProject().bind(this)
+            this.addProject.bind(this)
+            this.renderProjectList().bind(this)
+        });
     },
     createProject: function() {
-        this.project = createDiv();
-        this.project.classList.add('project')
+        this.project = new Project('john')
     },
     renderProjectList: function () {
+        while (this.projectList.firstChild) {
+            this.projectList.removeChild(this.projectList.firstChild);
+        }
+        for(let i = 0; i < projectList.length; i++) {
+            
+        }
+        console.log(this.projectListArr)
 
     }
 }
