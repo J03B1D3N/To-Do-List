d90396a7ee036d86a67bec63b407dcf807f4b269
Justas Lapiene <Justas63@gmail.com>
2022-12-16 12:29:08 +0100

    started building a revealing module for projectlist

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
 import './style.scss'
-import { cacheDom } from './cachingDom'
+
+(function() {
+    let todoApp = {
+        projectList : [],
+        toDoList: [],
+    init: function() {
+        this.cacheDom();
+        this.bindEvents();
+        this.shorts();
+        this.renderProjectList();
+    },
+    shorts: function() {
+        function createDiv() {
+            document.createElement('div')
+        }
+    },
+    cacheDom: function() {
+        this.addProject = document.getElementById('add')
+        this.projectList = document.getElementById('projectList')
+        this.deleteProject = document.getElementById('delete')
+    },
+    bindEvents: function() {
+        this.addProject.addEventListener('click', (this.addProject))
+    },
+    addProject: function() {
+        this.createProject();
+        projectList.push(this.project)
+    },
+    createProject: function() {
+        this.project = createDiv();
+        this.project.classList.add('project')
+    },
+    renderProjectList: function () {
+
+    }
+}
+todoApp.init();
+})()
