8e74fa03cff8bce97730d0783a33adb604bfc4cb
Justas Lapiene <Justas63@gmail.com>
2022-12-16 11:50:23 +0100

    finsihed styling nav with projects

---
+++ src/cachingDom.js
@@ -0,0 +1,5 @@
+export function cacheDom() {
+    const addProject = document.getElementById('add')
+    const projectList = document.getElementById('projectList')
+    const deleteProject = document.getElementById('delete')
+}
