e134c74983acaca2fa71f1a52fa641eb5ef2fd29
Justas Lapiene <Justas63@gmail.com>
2022-12-17 18:19:14 +0100

    finished add Task form css, trying to get the task render function working

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './style.scss';
     
     let todoApp = {
         projectListArr : [],
-        toDoList: [],
     init: function() {
         this.cacheDom();
         this.bindEvents();
@@ -20,14 +19,17 @@ import './style.scss';
         this.projectTitleInputDiv = document.getElementById('titleBtns')
         this.projectTitleInputConfirm = document.getElementById('submitTitleInput')
         this.projectTitleInputCancel = document.getElementById('cancelTitleInput')
+        this.addToDoBtn = document.getElementById('addToDo')
+        this.toDoInputFieldForm = document.getElementById('toDoInputField')
+        this.toDoInputField = document.getElementById('toDoInput')
+        this.cancelToDoInputBtn = document.getElementById('cancelToDoInput')
+        this.toDoInputDate = document.getElementById('toDoInputDate')
+        this.toDoListDOM = document.getElementById('toDoList')
     },
     bindEvents: function() {
-        this.addProjectBtn.addEventListener('click', (e) => {
-            this.projectTitleInputDiv.style.display = 'flex'
+           this.addProjectBtn.addEventListener('click', (e) => {
+           this.projectTitleInputDiv.style.display = 'flex'
            this.projectTitleInput.focus()
-            // this.createProject()
-            // this.addProject()
-            // this.renderProjectList()
         });
         this.projectTitleForm.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -36,30 +38,34 @@ import './style.scss';
             this.projectTitleInput.value = ''
             this.projectTitleInputDiv.style.display = 'none'
             this.renderProjectList();
+            console.log(this.projectListArr)
         }),
-        this.projectTitleInputConfirm.addEventListener('submit', () => {
-            this.createProject();
-            this.addProject();
-            this.projectTitleInput.value = ''
-            this.projectTitleInputDiv.style.display = 'none'
-            this.renderProjectList();
-        }),
-        this.projectTitleInputCancel.addEventListener('click', () => {
+        this.projectTitleInputCancel.addEventListener('click', (e) => {
+            e.preventDefault();
             this.projectTitleInput.value = ''
             this.projectTitleInputDiv.style.display = 'none'
         })
-        
-    },
-    addProject: function() {
-        this.projectListArr.push(this.project)
+        this.addToDoBtn.addEventListener('click', () => {
+            this.toDoInputFieldForm.style.display = 'flex'
+            this.addToDoBtn.style.display = 'none'
+        })
+        this.toDoInputFieldForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this.createToDo();
+          
+        }) 
     },
     createProject: function() {
         class Project {
             constructor  (projectTitleInput) {
                 this.title = projectTitleInput
+                this.toDos = ['add ToDo']
             }
             }
         this.project = new Project(this.projectTitleInput.value)
+    },
+    addProject: function() {
+        this.projectListArr.push(this.project)
     },
     deleteProject: function(event) {
         const target = event.currentTarget.parentNode
@@ -77,6 +83,7 @@ import './style.scss';
             div.setAttribute('data-id', `${i}`)
             const title = document.createElement('div')
             title.classList.add('title')
+            title.addEventListener('click', (e) => {this.renderToDo(e)})
             div.classList.add('project')
             const deleteBtn = document.createElement('button')
             deleteBtn.classList.add('delete')
@@ -86,7 +93,29 @@ import './style.scss';
             title.textContent = this.projectListArr[i].title
             this.projectList.appendChild(div)
         }
+    },
+    createToDo: function () {
+        class Task {
+            constructor (title, date) {
+                this.title = title;
+                this.date = date;
+            }
+        }
+        this.toDo = new Task(this.toDoInputField.value, this.toDoInputDate.value)
+    },
+
+    addToDo: function (n) {
+        this.projectListArr[n].toDos.push(this.toDo)
+    },
+
+    renderToDo: function () {
+        while (this.toDoListDOM.firstChild) {
+            this.toDoListDOM.removeChild(this.toDoListDOM.firstChild);
+        }
     }
+    
+   
+    
 }
 todoApp.init();
 })()
